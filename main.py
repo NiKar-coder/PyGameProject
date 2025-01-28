7d7886a6cac8f20d6d2588e17bf8340a7b0d72fb
NiKar-coder <Nikitakarataev22@gmail.com>
2025-01-28 18:33:15 +0300

    Update skydiver

--- main.py
+++ main.py
@@ -549,7 +549,7 @@ def main():
                 running = False
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_RIGHT:
-                    skydiver.rect.x = WIDTH - 63
+                    skydiver.rect.x = WIDTH - 61
                 if event.key == pg.K_LEFT:
                     skydiver.rect.x = 10
                 if event.key == pg.K_DOWN:
