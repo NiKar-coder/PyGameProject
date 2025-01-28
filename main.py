c1ffd691d35db15e6c86633669fd13286776543b
NiKar-coder <Nikitakarataev22@gmail.com>
2025-01-28 17:58:00 +0300

    Update scores' color

--- main.py
+++ main.py
@@ -490,7 +490,7 @@ font_name = pg.font.match_font(FONT_)
 
 def draw_text(surf, text, size, x, y):
     font = pg.font.Font(font_name, size)
-    text_surface = font.render(text, True, (255, 0, 0))
+    text_surface = font.render(text, True, (203, 0, 255))
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
